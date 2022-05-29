1cdaf35255a10f02744e3ebb9de65d5372f2c687
y-nakata83 <a.and.mi.0815@gmail.com>
2022-05-29 21:25:58 +0900

    共有

---
+++ js/main.js
@@ -0,0 +1,228 @@
+// 波線で風をイメージ
+
+var unit = 100,
+    canvasList, // キャンバスの配列
+    info = {}, // 全キャンバス共通の描画情報
+    colorList; // 各キャンバスの色情報
+
+/**
+ * Init function.
+ * 
+ * Initialize variables and begin the animation.
+ */
+function init() {
+    info.seconds = 0;
+    info.t = 0;
+		canvasList = [];
+    colorList = [];
+    // canvas1個めの色指定
+    canvasList.push(document.getElementById("waveCanvas"));
+    colorList.push(['#73F7DD', '#8FF8E3', '#80DBE0', '#56CFD5', '#2CC4CB']);//重ねる波線の色設定
+    
+	
+		// 各キャンバスの初期化
+		for(var canvasIndex in canvasList) {
+        var canvas = canvasList[canvasIndex];
+        canvas.width = document.documentElement.clientWidth; //Canvasのwidthをウィンドウの幅に合わせる
+        canvas.height = 300;//波の高さ
+        canvas.contextCache = canvas.getContext("2d");
+    }
+    // 共通の更新処理呼び出し
+		update();
+}
+
+function update() {
+		for(var canvasIndex in canvasList) {
+        var canvas = canvasList[canvasIndex];
+        // 各キャンバスの描画
+        draw(canvas, colorList[canvasIndex]);
+    }
+    // 共通の描画情報の更新
+    info.seconds = info.seconds - .012;
+    info.t = info.seconds*Math.PI;
+    // 自身の再起呼び出し
+    setTimeout(update, 35);
+}
+
+/**
+ * Draw animation function.
+ * 
+ * This function draws one frame of the animation, waits 20ms, and then calls
+ * itself again.
+ */
+function draw(canvas, color) {
+		// 対象のcanvasのコンテキストを取得
+    var context = canvas.contextCache;
+    // キャンバスの描画をクリア
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    //波線を描画 drawWave(canvas, color[数字（波の数を0から数えて指定）], 透過, 波の幅のzoom,波の開始位置の遅れ )
+    drawWave(canvas, color[0], 0.8, 3, 0);
+	drawWave(canvas, color[1], 0.5, 4, 0);
+	drawWave(canvas, color[2], 0.5, 1.6, 0);
+	drawWave(canvas, color[3], 0.5, 3, 100);
+	drawWave(canvas, color[4], 0.5, 1.6, 200);
+}
+
+/**
+* 波を描画
+* drawWave(色, 不透明度, 波の幅のzoom, 波の開始位置の遅れ)
+*/
+function drawWave(canvas, color, alpha, zoom, delay) {
+	var context = canvas.contextCache;
+    context.strokeStyle = color;//線の色
+	context.lineWidth = 1;//線の幅
+    context.globalAlpha = alpha;
+    context.beginPath(); //パスの開始
+    drawSine(canvas, info.t / 0.5, zoom, delay);
+    context.stroke(); //線
+}
+
+/**
+ * Function to draw sine
+ * 
+ * The sine curve is drawn in 10px segments starting at the origin. 
+ * drawSine(時間, 波の幅のzoom, 波の開始位置の遅れ)
+ */
+function drawSine(canvas, t, zoom, delay) {
+    var xAxis = Math.floor(canvas.height/2);
+    var yAxis = 0;
+    var context = canvas.contextCache;
+    // Set the initial x and y, starting at 0,0 and translating to the origin on
+    // the canvas.
+    var x = t; //時間を横の位置とする
+    var y = Math.sin(x)/zoom;
+    context.moveTo(yAxis, unit*y+xAxis); //スタート位置にパスを置く
+
+    // Loop to draw segments (横幅の分、波を描画)
+    for (i = yAxis; i <= canvas.width + 10; i += 10) {
+        x = t+(-yAxis+i)/unit/zoom;
+        y = Math.sin(x - delay)/3;
+        context.lineTo(i, unit*y+xAxis);
+    }
+}
+
+init();
+
+
+// 桜が散るやつ
+
+particlesJS("particles-js", {
+	"particles":{
+		"number":{
+			"value":30,//この数値を変更すると桜の数が増減できる
+			"density":{
+				"enable":true,
+				"value_area":1121.6780303333778
+			}
+		},
+		"color":{
+			"value":"#fff"
+		},
+		"shape":{
+			"type":"image",//形状は画像を指定
+			"stroke":{
+				"width":0,
+			},
+			"image":{
+				"src":"img/flower.png",//【重要】画像を指定！桜の画像を設定してください。
+				"width":120,
+				"height":120
+			}
+		},
+		"opacity":{
+			"value":0.06409588744762158,
+			"random":true,
+			"anim":{
+				"enable":false,
+				"speed":1,
+				"opacity_min":0.1,
+				"sync":false
+			}
+		},
+		"size":{
+			"value":8.011985930952697,
+			"random":true,//サイズをランダムに
+			"anim":{
+				"enable":false,
+				"speed":4,
+				"size_min":0.1,
+				"sync":false
+			}
+		},
+		"line_linked":{
+			"enable":false,
+		},
+		"move":{
+			"enable":true,
+			"speed":7,//この数値を小さくするとゆっくりな動きになる
+			"direction":"bottom-right",//右下に向かって落ちる
+			"random":false,//動きはランダムにしない
+			"straight":false,//動きをとどめない
+			"out_mode":"out",//画面の外に出るように描写
+			"bounce":false,//跳ね返りなし
+			"attract":{
+				"enable":false,
+				"rotateX":281.9177489524316,
+				"rotateY":127.670995809726
+			}
+		}
+	},
+	"interactivity":{
+		"detect_on":"canvas",
+		"events":{
+			"onhover":{
+				"enable":false,
+			},
+			"onclick":{
+				"enable":false,
+			},
+			"resize":true
+		}
+	},
+	"retina_detect":false
+});
+
+
+
+$(window).on('load',function(){
+$("#splash-logo").delay(1200).fadeOut('slow');//ロゴを1.2秒でフェードアウトする記述
+
+//=====ここからローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+$("#splash").delay(3000).fadeOut('slow',function(){//ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
+
+$('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
+
+});
+//=====ここまでローディングエリア（splashエリア）を1.5秒でフェードアウトした後に動かしたいJSをまとめる
+
+//=====ここから背景が伸びた後に動かしたいJSをまとめたい場合は
+$('.splashbg').on('animationend', function() { 
+//この中に動かしたいJSを記載
+  function slideAnime(){
+	//====左に動くアニメーションここから===
+		$('.leftAnime').each(function(){ 
+			var elemPos = $(this).offset().top-50;
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll >= elemPos - windowHeight){
+				//左から右へ表示するクラスを付与
+				//テキスト要素を挟む親要素（左側）とテキスト要素を元位置でアニメーションをおこなう
+				$(this).addClass("slideAnimeLeftRight"); //要素を左枠外にへ移動しCSSアニメーションで左から元の位置に移動
+				$(this).children(".leftAnimeInner").addClass("slideAnimeRightLeft");  //子要素は親要素のアニメーションに影響されないように逆の指定をし元の位置をキープするアニメーションをおこなう
+			}else{
+				//左から右へ表示するクラスを取り除く
+				$(this).removeClass("slideAnimeLeftRight");
+				$(this).children(".leftAnimeInner").removeClass("slideAnimeRightLeft");
+				
+			}
+		});
+		
+}
+slideAnime();/* アニメーション用の関数を呼ぶ*/
+  
+});
+//=====ここまで背景が伸びた後に動かしたいJSをまとめる
+
+});
+
