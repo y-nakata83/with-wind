a2e4d1e821032b80f57b966454fe4cde134356a8
y-nakata83 <a.and.mi.0815@gmail.com>
2022-05-29 23:01:38 +0900

    js変更

--- js/main.js
+++ js/main.js
@@ -199,31 +199,30 @@ $('body').addClass('appear');//フェードアウト後bodyにappearクラス付
 //=====ここから背景が伸びた後に動かしたいJSをまとめたい場合は
 $('.splashbg').on('animationend', function() { 
 //この中に動かしたいJSを記載
-
+  function slideAnime(){
+	//====左に動くアニメーションここから===
+		$('.leftAnime').each(function(){ 
+			var elemPos = $(this).offset().top-50;
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll >= elemPos - windowHeight){
+				//左から右へ表示するクラスを付与
+				//テキスト要素を挟む親要素（左側）とテキスト要素を元位置でアニメーションをおこなう
+				$(this).addClass("slideAnimeLeftRight"); //要素を左枠外にへ移動しCSSアニメーションで左から元の位置に移動
+				$(this).children(".leftAnimeInner").addClass("slideAnimeRightLeft");  //子要素は親要素のアニメーションに影響されないように逆の指定をし元の位置をキープするアニメーションをおこなう
+			}else{
+				//左から右へ表示するクラスを取り除く
+				$(this).removeClass("slideAnimeLeftRight");
+				$(this).children(".leftAnimeInner").removeClass("slideAnimeRightLeft");
+				
+			}
+		});
+		
+}
+slideAnime();/* アニメーション用の関数を呼ぶ*/
+  
 });
 //=====ここまで背景が伸びた後に動かしたいJSをまとめる
 
 });
 
-
-function slideAnime(){
-//====左に動くアニメーションここから===
-	$('.leftAnime').each(function(){ 
-		var elemPos = $(this).offset().top-50;
-		var scroll = $(window).scrollTop();
-		var windowHeight = $(window).height();
-		if (scroll >= elemPos - windowHeight){
-			//左から右へ表示するクラスを付与
-			//テキスト要素を挟む親要素（左側）とテキスト要素を元位置でアニメーションをおこなう
-			$(this).addClass("slideAnimeLeftRight"); //要素を左枠外にへ移動しCSSアニメーションで左から元の位置に移動
-			$(this).children(".leftAnimeInner").addClass("slideAnimeRightLeft");  //子要素は親要素のアニメーションに影響されないように逆の指定をし元の位置をキープするアニメーションをおこなう
-		}else{
-			//左から右へ表示するクラスを取り除く
-			$(this).removeClass("slideAnimeLeftRight");
-			$(this).children(".leftAnimeInner").removeClass("slideAnimeRightLeft");
-			
-		}
-	});
-	
-}
-slideAnime();/* アニメーション用の関数を呼ぶ*/
